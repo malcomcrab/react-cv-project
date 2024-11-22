435b80bc48543000845b9c72ce36edec2c0e42cf
malcomcrab <malcomcrabesq@outlook.com>
2024-11-22 14:50:15 +0000

    Tested mapping an array of ojbects through the input display component and passed props to children when creating elements.

--- src/components/InputDisplay.jsx
+++ src/components/InputDisplay.jsx
@@ -1,13 +1,15 @@
 import Button from './Button';
 import inputIcon from '../assets/editIcon.svg'
+import Input from './Input';
 
 function InputDisplay(props){
 
     return(
-        <>
+        <div>
+            <Input type={props.type} value={props.value}/>
+            <Button value={'Submit'}/>
             <p>{props.value} <Button value={<img src={inputIcon}></img>} /></p>
-
-        </>
+        </div>
     )
 }
 
