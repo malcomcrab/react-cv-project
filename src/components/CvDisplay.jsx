3aaa3cb17040ef211ccd9dc69321c7905b8018ed
malcomcrab <malcomcrabesq@outlook.com>
2024-11-26 17:04:17 +0000

    Added some basic CSS formatting in for clarity

--- src/components/CvDisplay.jsx
+++ src/components/CvDisplay.jsx
@@ -2,13 +2,16 @@
 function CvDisplay({firstName, surname}){
 
     return(
-        <div className="cv-display-container">
-            
-            <div className="cv-names-container">
+        <div id="cv-display-container">
+
+            <div id="cv-names-container">
                 <p id="first-name-display" className="name-display">{firstName}</p>
                 <p id="surname-display" className="name-display">{surname}</p>
             </div>
-
+            <div>
+                <hr/>
+            </div>
+            
         </div>
     )
 
